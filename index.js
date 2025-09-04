fde73d719c2bdd71420e2feeb076de2450630c06
Wasim <princewasim971@gmail.com>
2025-09-04 08:59:52 +0500

    updated index file

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-import app from "./server.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
-export default app;
+import staffRoutes from "./routes/staffRoutes.js";
+import hotelRoutes from "./routes/hotelRoutes.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
+
+import errorHandler from "./middleware/errorHandler.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+const API_VERSION = "/api/v0";
+
+app.use(`${API_VERSION}/staff`, staffRoutes);
+app.use(`${API_VERSION}/hotels`, hotelRoutes);
+app.use(`${API_VERSION}/appointments`, appointmentRoutes);
+// Error Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
